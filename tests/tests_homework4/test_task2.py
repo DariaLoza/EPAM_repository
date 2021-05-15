07ef0fbe59fbdc7a69c380139a88b03406b669b4
daria <dasha.loza24@gmail.com>
2021-05-15 20:03:44 +0300

    Please check homework4

---
+++ tests/tests_homework4/test_task2.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import pytest
+from homeworks.homework4.task2 import count_dots_on_i
+from mock import patch
+
+
+def test_count_dots_on_i_without_monkey_patch():
+    assert count_dots_on_i("https://example.com/") == 59
+
+
+class FetchTests(TestCase):
+    def test_count_dots_on_i_with_monkey_patch(self):
+        with patch("urllib.request.urlopen") as mock_request:
+            url = "smth"
+
+            mock_request.return_value.response = "iiiiii"
+
+            self.assertTrue(self, count_dots_on_i(url) == 6)
