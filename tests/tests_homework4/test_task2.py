ac46069ca6736f1805b47f81c257239ff5b57d8c
daria <dasha.loza24@gmail.com>
2021-05-15 20:40:44 +0300

    edited tests

--- tests/tests_homework4/test_task2.py
+++ tests/tests_homework4/test_task2.py
@@ -5,11 +5,13 @@ from homeworks.homework4.task2 import count_dots_on_i
 
 
 def test_count_dots_on_i_without_monkey_patch():
+    """Testing test_count_dots_on_i without monkey patch"""
     assert count_dots_on_i("https://example.com/") == 59
 
 
 class FetchTests(TestCase):
     def test_count_dots_on_i_with_monkey_patch(self):
+        """Testing test_count_dots_on_i with monkey patch"""
         with mock.patch("urllib.request.urlopen") as mock_request:
             url = "smith"
 
@@ -18,6 +20,7 @@ class FetchTests(TestCase):
             self.assertTrue(self, count_dots_on_i(url) == 3)
 
     def test_count_dots_no_i_with_monkey_patch(self):
+        """Testing test_count_dots_on_i without i"""
         with mock.patch("urllib.request.urlopen") as mock_request:
             url = "smith"
 
