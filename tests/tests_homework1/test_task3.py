fab672135c06bba77560c560ce93e5d936cdd53f
daria <dasha.loza24@gmail.com>
2021-05-17 12:52:49 +0300

    add full path in test_task3

--- tests/tests_homework1/test_task3.py
+++ tests/tests_homework1/test_task3.py
@@ -3,9 +3,9 @@ from homeworks.homework1.task3 import find_maximum_and_minimum
 
 def test_1():
     """Testing check1"""
-    assert find_maximum_and_minimum("check1.txt") == (1, 9)
+    assert find_maximum_and_minimum("tests/tests_homework1/check1.txt") == (1, 9)
 
 
 def test_2():
     """Testing check2"""
-    assert find_maximum_and_minimum("check2.txt") == (-6, 9)
+    assert find_maximum_and_minimum("tests/tests_homework1/check2.txt") == (-6, 9)
