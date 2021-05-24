8fcf6db80655e129a9c4e2e50ec1db19d1472ada
daria <dasha.loza24@gmail.com>
2021-05-24 15:07:01 +0300

    edited test_task1

--- tests/tests_homework5/test_task1.py
+++ tests/tests_homework5/test_task1.py
@@ -1,19 +1,30 @@
-from homeworks.homework5.task1 import Student, Teacher
+import datetime
+
+import pytest
+
+from homeworks.homework5.task1 import Homework, Student, Teacher
 
 
-def test_task_1():
-    if __name__ == "__main__":
-        teacher = Teacher("Daniil", "Shadrin")
-        student = Student("Roman", "Petrov")
-        assert teacher.last_name == "Shadrin"
-        assert student.first_name == "Roman"
-        expired_homework = teacher.create_homework("Learn functions", 0)
-        assert expired_homework.created == "2021-05-17 16:34:23.323787"
-        assert expired_homework.deadline == "0:00:00"
-        assert expired_homework.text == "Learn functions"
-        # create function from method and use it
-        create_homework_too = teacher.create_homework
-        oop_homework = create_homework_too("create 2 simple classes", 5)
-        assert oop_homework.deadline == "5 days, 0:00:00"
-        assert student.do_homework(oop_homework) == "You are late" "None"
-        assert student.do_homework(expired_homework) == "You are late" "None"
+def test_creating_homework():
+    teacher = Teacher("Daniil", "Shadrin")
+    expired_homework = teacher.create_homework("Learn functions", 0)
+    assert isinstance(expired_homework, Homework)
+    assert expired_homework.deadline == datetime.timedelta(0)
+    assert expired_homework.text == "Learn functions"
+
+
+def test_creating_function_from_method_and_using_it():
+    teacher = Teacher("Daniil", "Shadrin")
+    create_homework_too = teacher.create_homework
+    oop_homework = create_homework_too("create 2 simple classes", 5)
+    assert oop_homework.deadline == datetime.timedelta(days=5)
+
+
+def test_do_homework(capsys):
+    teacher = Teacher("Daniil", "Shadrin")
+    expired_homework = teacher.create_homework("Learn functions", 0)
+    student = Student("Roman", "Petrov")
+    student.do_homework(expired_homework)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "You are late"
+
