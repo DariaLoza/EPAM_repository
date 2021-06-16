d47ab5832a496cd4b8ab96ac5b8ac14aa209b98b
daria <dasha.loza24@gmail.com>
2021-06-16 14:44:18 +0300

    edited test_task1,test_task3

--- tests/tests_homework9/test_task3.py
+++ tests/tests_homework9/test_task3.py
@@ -2,8 +2,15 @@ from homeworks.homework9.task3 import universal_file_counter
 
 
 def test_universal_file_counter_without_tokenizer():
-    assert universal_file_counter("tests/tests_homework9/test_file_HW_9_task3", "txt") == 4
+    assert (
+        universal_file_counter("tests/tests_homework9/test_file_HW_9_task3", "txt") == 4
+    )
 
 
 def test_universal_file_counter_with_tokenizer():
-    assert universal_file_counter("tests/tests_homework9/test_file_HW_9_task3", "txt", str.split) == 4
+    assert (
+        universal_file_counter(
+            "tests/tests_homework9/test_file_HW_9_task3", "txt", str.split
+        )
+        == 4
+    )
