b502b8cea79caa7c1f4aa88aac51419f1c2a1f92
daria <dasha.loza24@gmail.com>
2021-06-16 14:08:37 +0300

    edited task1 (add Path) and test_task1(add full path)

--- tests/tests_homework8/test_task1.py
+++ tests/tests_homework8/test_task1.py
@@ -2,11 +2,11 @@ from homeworks.homework8.task1 import KeyValueStorage
 
 
 def test_KeyValueStorage_as_element():
-    storage = KeyValueStorage("key_value_storage")
+    storage = KeyValueStorage("tests/tests_homework8/key_value_storage")
     assert storage["name"] == "kek"
 
 
 def test_KeyValueStorage_as_attribute():
-    storage = KeyValueStorage("key_value_storage")
+    storage = KeyValueStorage("tests/tests_homework8/key_value_storage")
     assert storage.song == "shadilay"
     assert storage.power == 9001
