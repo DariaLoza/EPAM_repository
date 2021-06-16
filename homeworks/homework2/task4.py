cbe529b8cae7d4d77b26fed6b76e25559687e26b
daria <dasha.loza24@gmail.com>
2021-06-16 15:44:00 +0300

    homework2

---
+++ homeworks/homework2/task4.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+
+def cache(cached_function):
+    stored_value = []
+
+    @wraps(cached_function)
+    def wrapped(*args, **kwargs):
+        nonlocal stored_value
+
+        key_of_call = (args, kwargs)
+        for key, value in stored_value:
+            if key == key_of_call:
+                return value
+        else:
+            result = cached_function(*args, **kwargs)
+            stored_value.append((key_of_call, result))
+            return result
+
+    return wrapped
+
+
+@cache
+def cached_function(a, b):
+    """Docstring of original function"""
+    return (a ** b) ** 2
---
+++ tests/tests_homework2/test_task1.py
@@ -0,0 +1,37 @@
+import homeworks.homework2.task1
+
+
+def test_get_longest_diverse_words():
+    """Testing that function find 10 longest words consisting from largest amount of unique symbols"""
+    assert homeworks.homework2.task1.get_longest_diverse_words("data.txt") == [
+        "Nichtkämpfern",
+        "Entscheidungsschlacht",
+        "Schöpfungsmacht",
+        "Werkstättenlandschaft",
+        "Schicksalsfiguren",
+        "politisch-strategischen",
+        "Selbstverständlich",
+        "résistance-Bewegungen",
+        "unmißverständliche",
+        "Bevölkerungsabschub",
+    ]
+
+
+def test_count_punctuation_chars():
+    """Testing that function count every punctuation char"""
+    assert homeworks.homework2.task1.count_punctuation_chars("data.txt") == 5305
+
+
+def test_get_rarest_char():
+    """Testing that function find rarest symbol for document"""
+    assert homeworks.homework2.task1.get_rarest_char("data.txt") == ")"
+
+
+def test_count_non_ascii_chars():
+    """Testing that function count every non ascii char"""
+    assert homeworks.homework2.task1.count_non_ascii_chars("data.txt") == 2972
+
+
+def test_get_most_common_non_ascii_char():
+    """Testing that function find most common non ascii char for document"""
+    assert homeworks.homework2.task1.get_most_common_non_ascii_char("data.txt") == "ä"
