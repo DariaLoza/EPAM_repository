9ca9f1fc4094e8979687d6f18c0607fc8deff51d
daria <dasha.loza24@gmail.com>
2021-06-16 21:22:35 +0300

    edited test_task1.py

--- tests/tests_homework1/test_task1.py
+++ tests/tests_homework1/test_task1.py
@@ -3,7 +3,7 @@ from homeworks.homework2.task1 import *
 
 def test_get_longest_diverse_words():
     """Testing that function find 10 longest words"""
-    assert get_longest_diverse_words("homeworks/homework2/data.txt") == [
+    assert get_longest_diverse_words("tests/tests_homework1/data.txt") == [
         "Nichtkämpfern",
         "Entscheidungsschlacht",
         "Schöpfungsmacht",
@@ -19,19 +19,19 @@ def test_get_longest_diverse_words():
 
 def test_count_punctuation_chars():
     """Testing that function count every punctuation char"""
-    assert count_punctuation_chars("homeworks/homework2/data.txt") == 5305
+    assert count_punctuation_chars("tests/tests_homework1/data.txt") == 5305
 
 
 def test_get_rarest_char():
     """Testing that function find rarest symbol for document"""
-    assert get_rarest_char("homeworks/homework2/data.txt") == ")"
+    assert get_rarest_char("tests/tests_homework1/data.txt") == ")"
 
 
 def test_count_non_ascii_chars():
     """Testing that function count every non ascii char"""
-    assert count_non_ascii_chars("homeworks/homework2/data.txt") == 2972
+    assert count_non_ascii_chars("tests/tests_homework1/data.txt") == 2972
 
 
 def test_get_most_common_non_ascii_char():
     """Testing that function find most common non ascii char"""
-    assert get_most_common_non_ascii_char("homeworks/homework2/data.txt") == "ä"
+    assert get_most_common_non_ascii_char("tests/tests_homework1/data.txt") == "ä"
