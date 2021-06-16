be30cf4379d5a2a07d987a8543b12a200fd28c41
daria <dasha.loza24@gmail.com>
2021-06-16 16:01:26 +0300

    homework4

---
+++ tests/tests_homework4/test_task1.py
@@ -0,0 +1,45 @@
+import os
+
+import pytest
+
+from homeworks.homework4.task1 import read_magic_number
+
+
+def test_read_magic_number_true():
+    """_Testing True result_"""
+    test_file_1 = os.open("file_1.txt", os.O_CREAT | os.O_WRONLY)
+    number = "2"
+    encoded_number = str.encode(number)
+    os.write(test_file_1, encoded_number)
+    path = os.path.abspath("file_1.txt")
+    if os.path.isfile(path):
+        assert read_magic_number(path)
+    os.close(test_file_1)
+    os.remove(path)
+
+
+def test_read_magic_number_false():
+    """_Testing False result_"""
+    test_file_2 = os.open("file_2.txt", os.O_CREAT | os.O_WRONLY)
+    number = "5"
+    encoded_number = str.encode(number)
+    os.write(test_file_2, encoded_number)
+    path = os.path.abspath("file_2.txt")
+    if os.path.isfile(path):
+        assert not read_magic_number(path)
+    os.close(test_file_2)
+    os.remove(path)
+
+
+def test_read_magic_number_value_error():
+    """_Testing ValueError result_"""
+    test_file_3 = os.open("file_3.txt", os.O_CREAT | os.O_WRONLY)
+    number = "t"
+    encoded_number = str.encode(number)
+    os.write(test_file_3, encoded_number)
+    path = os.path.abspath("file_3.txt")
+    if os.path.isfile(path):
+        with pytest.raises(ValueError):
+            assert read_magic_number(path) == ValueError
+    os.close(test_file_3)
+    os.remove(path)
---
+++ tests/tests_homework4/test_task4.py
@@ -0,0 +1,5 @@
+from homeworks.homework4.task4 import fizzbuzz
+
+
+def test_fizzbuzz_function_with_5():
+    assert fizzbuzz(5) == ["1", "2", "fizz", "4", "buzz"]
